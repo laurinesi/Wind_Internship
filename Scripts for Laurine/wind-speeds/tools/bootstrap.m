84e82365265d4147018a13b8866d10f41529bd42
laurine <l.simonetti@outlook.fr>
2024-10-02 09:44:51 +0200

    Update bootstrap.m
    Bootstrap with or without the tail parameter fixed

--- Scripts for Laurine/wind-speeds/tools/bootstrap.m
+++ Scripts for Laurine/wind-speeds/tools/bootstrap.m
@@ -8,10 +8,12 @@ function [GEVparameters] = bootstrap(population, n, tail)
 
 % Initialization
 samples = zeros(length(population), n);
-parmhat = zeros(n, 3);  % [k, sigma, mu] parameters of GEV
+% parmhat = zeros(n, 3);  % [k, sigma, mu] parameters of GEV
 
 % if the tail is not in the input
 if nargin < 3 || isempty(tail)
+
+    parmhat = zeros(n, 3);
     % Generate bootstrap samples and fit GEV distribution to each
     for i = 1:n
         % Generate a bootstrap sample with replacement
@@ -30,25 +32,26 @@ if nargin < 3 || isempty(tail)
     disp(['ndraw = ', num2str(length(population)), 'years'])
     disp(GEVparameters);
 
-else
+else % if the tail is fixed
 
-% Generate bootstrap samples and fit GEV distribution to each
-for i = 1:n
-    % Generate a bootstrap sample with replacement
-    samples(:, i) = datasample(population, length(population), 'Replace', true);
+    parmhat = zeros(n, 2);
+    % Generate bootstrap samples and fit GEV distribution to each
+    for i = 1:n
+        % Generate a bootstrap sample with replacement
+        samples(:, i) = datasample(population, length(population), 'Replace', true);
+        
+        % Fit GEV distribution to the bootstrap sample
+        % gevfit(X) returns maximum likelihood estimates
+        parmhat(i, :) = gevfit_fixedtail(samples(:, i),tail);
+    end
     
-    % Fit GEV distribution to the bootstrap sample
-    % gevfit(X) returns maximum likelihood estimates
-    parmhat(i, :) = gevfit_fixedtail(samples(:, i),tail);
-end
-
-GEVparameters = array2table(parmhat, "VariableNames",{'scale', 'location'});
-
-% Display the GEV parameters for each bootstrap sample
-disp('GEV Parameters for each bootstrap sample :');
-disp(['Result for ', num2str(n), ' estimations'])
-disp(['ndraw = ', num2str(length(population)), 'years'])
-disp(GEVparameters);
+    GEVparameters = array2table(parmhat, "VariableNames",{'scale', 'location'});
+    
+    % Display the GEV parameters for each bootstrap sample
+    disp('GEV Parameters for each bootstrap sample :');
+    disp(['Result for ', num2str(n), ' estimations'])
+    disp(['ndraw = ', num2str(length(population)), 'years'])
+    disp(GEVparameters);
 
 end
 
